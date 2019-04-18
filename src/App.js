b9734fdaaafa5e9b446c5a67e6af505ba78218ef
unknown <theitalianpeter@hotmail.com>
2019-04-18 23:48:35 +0100

    little add commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React, { Component } from 'react';
+import React,{useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+const App = () => {
+  const APP_ID = '4110510d';
+  const APP_KEY = "cdaa5e93404341e633f5e3a69ff7ced1";
+  
+  const exampleReq = 
+  'https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}'
 
-      </div>
-    );
-  }
-}
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() =>{
+    console.log('Effect has been run');
+  });
+
+
+  return(
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button 
+          onClick={() => setCounter(counter + 1) } 
+          className="search-button" type="submit"
+          >
+          {counter}
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default App;
